b936c6c940663324270167ffcaa1cfb45b071df7
simone-art <simoneperiodista3@gmail.com>
2019-11-14 15:47:52 -0300

    finalizada  la revisión del ejercicio de React

---
+++ src/componentes/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render(){
+        return(
+            <header className = "cabecalho" >
+                <h1> texto</h1>
+            </header>
+        )
+    }
+}
+
+export default Header 
